772a11d50bc76e87fb222289a1363a81d4878e50
Joshua Arber <arber@unlv.nevada.edu>
2020-09-12 13:28:20 -0700

    re-factored functions into book prototypes and added remove book button function

--- library.js
+++ library.js
@@ -1,7 +1,10 @@
+//-----------------------------------------------/
+// library functions
 
 const theLibrary = [];
+const shelf = document.getElementById('shelf');
 
-// constructor
+// book constructor
 function Book(title, author, pages, didRead) {
     this.title = title;
     this.author = author;
@@ -13,8 +16,78 @@ Book.prototype.info = function() {
     return this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + (this.didRead ? 'read' : 'not read');
 }
 
-function addBookToLibrary(book) {
-    return theLibrary.push(book);
+Book.prototype.createBookCard = function() {
+    let card = document.createElement('div');
+    card.className = 'card';
+    card.setAttribute('data-index', theLibrary.indexOf(this));
+
+    let x = Object.getOwnPropertyNames(this);
+    for (let i = 0; i < x.length; i++) {
+        this.addInfoToCard(x[i], card);
+    }
+
+    card.appendChild(this.createRemoveBtn());
+    shelf.appendChild(card);
+    this.bookCard = card;
+}
+
+Book.prototype.addInfoToCard = function(property, card) {
+    let p = document.createElement('p');
+
+    if (property == 'pages') {
+        p.className = property;
+        p.innerHTML = this[property] + ' pages';
+    }
+    else if (property == 'didRead') {
+        p.className = p.innerHTML = this[property] ? 'finished' : 'unfinished';
+    }
+    else {
+        p.className = property;
+        p.innerHTML = this[property];
+    }
+
+    return card.appendChild(p);
+}
+
+Book.prototype.removeBookFromLibrary = function() {
+    return theLibrary.splice(theLibrary.indexOf(this), 1);
+}
+
+Book.prototype.createRemoveBtn = function() {
+    let rm = document.createElement('input');
+    rm.type = 'button';
+    rm.value = 'remove';
+    rm.className = 'remove-book-btn';
+    let thisBook = this;
+    rm.addEventListener('click', function() {
+        let bookIndex = theLibrary.indexOf(thisBook);
+        let bookNode = document.querySelector(`[data-index="${bookIndex}"]`)
+        document.querySelector('.card').parentNode.removeChild(bookNode);
+        theLibrary.splice(bookIndex, 1);
+        //console.log(theLibrary);
+    });
+    
+    return rm;
+}
+
+Book.prototype.getBookCard = function() {
+    return document.querySelector(`[data-index="${theLibrary.indexOf(this)}"]`);
+}
+
+Book.prototype.isInLibrary = function() {
+    return theLibrary.includes(this);
+}
+
+Book.prototype.addToLibrary = function() {
+    if (!this.isInLibrary()) {
+        theLibrary.push(this);
+        this.createBookCard();
+    }
+    else {
+        'this book is already in the library!';
+    }
+
+    return this;
 }
 
 function printLibrary() {
@@ -23,33 +96,8 @@ function printLibrary() {
     });
 }
 
-function appendBookCard(book) {
-    let bookCard = document.createElement('div');
-    bookCard.className = 'card';
-    let bookDetails = ['title', 'author', 'pages', 'didRead']
-    for (let i = 0 ; i < bookDetails.length; i++) {
-        addDetailToCard(book, bookDetails[i], bookCard);
-    }
-    shelf.appendChild(bookCard);
-}
-
-function addDetailToCard(book, detailType, card) {
-    let p = document.createElement('p');
-
-    if (detailType == 'pages') {
-        p.className = detailType;
-        p.innerHTML = book[detailType] + ' pages';
-    }
-    else if (detailType == 'didRead') {
-        p.className = p.innerHTML = book[detailType] ? 'finished' : 'unfinished';
-    }
-    else {
-        p.className = detailType;
-        p.innerHTML = book[detailType];
-    }
-
-    card.appendChild(p);
-}
+//-----------------------------------------------/
+// DOM manipulation functions
 
 function addBookButton() {
     let x = document.getElementById('add-book-form');
@@ -64,26 +112,24 @@ function addBookButton() {
             info.push(x[i].value === 'yes' ? true : false);
         }
         else {
-            console.log('no input was given');
+            //console.log('no input was given');
         }
     }
 
     if (!info.includes('')) {
         let b = new Book(info[0], info[1], info[2], info[3]);
-        addBookToLibrary(b);
-        appendBookCard(b);
-        //console.log(b);
+        console.log(b.addToLibrary());
     }
-    else {
+    /*else {
         console.log('at least one of the fields is empty');
-    }
+    }*/
 
     document.getElementById('book-form').style.display = 'none';
     document.getElementById('add-btn').style.display = 'block';
-    
 }
 
 function openBookForm() {
     document.getElementById('book-form').style.display = 'block';
     document.getElementById('add-btn').style.display = 'none';
+    document.getElementById('add-book-form').reset();
 }
--- main.js
+++ main.js
@@ -1,19 +1,10 @@
-const shelf = document.getElementById('shelf');
 
 let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 let theWitcher = new Book('The Last Wish', 'Andrzej Sapkowski', 288, true);
 
-addBookToLibrary(theHobbit);
-addBookToLibrary(theWitcher);
-addBookToLibrary(theHobbit);
-addBookToLibrary(theWitcher);
-addBookToLibrary(theHobbit);
-addBookToLibrary(theWitcher);
-addBookToLibrary(theHobbit);
-addBookToLibrary(theWitcher);
 
-theLibrary.forEach(book => {
-    appendBookCard(book);
-    //console.log(book);
-});
 
+console.log(theHobbit.addToLibrary());
+console.log(theWitcher.addToLibrary());
+//console.log(theHobbit.getBookCard());
+//console.log(theWitcher.getBookCard());
